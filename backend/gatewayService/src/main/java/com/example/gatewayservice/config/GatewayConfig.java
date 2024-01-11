ccf0f038bb910143e587c88c89f062a611358804
Akram.ABDELHAK <Akram.ABDELHAK@ilemgroup.com>
2024-01-11 16:00:33 +0100

    configuration and create tables

---
+++ backend/gatewayService/src/main/java/com/example/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.example.gatewayservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class GatewayConfig {
+
+    //configuration des route dynamique
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("ms1-services", r -> r
+                        .path("/api/field/**")
+                        .or()
+                        //.path("api/catagory/**")
+                        .uri("lb://ms1-services") // doit etre exactement meme nom qui se trouve sur eurika
+                ).build();
+    }
+}
---
+++ backend/ms1/src/main/java/com/example/ms1/entities/Field.java
@@ -0,0 +1,28 @@
+package com.example.ms1.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Field {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int surface;
+    private String address;
+    @ManyToOne
+    private Category category;
+    @OneToMany(mappedBy = "field")
+    @JsonIgnore
+    private List<TnbTax> tnbTaxes;
+    @ManyToOne
+    @JsonIgnore
+    private Liable liable;
+
+}
