c02853a734aeb879807955ce87ade6a9a6d6c466
Akram.ABDELHAK <Akram.ABDELHAK@ilemgroup.com>
2024-01-14 17:44:18 +0100

    crud with forms in angular

--- frontend/src/app/components/category-list/category-list.component.ts
+++ frontend/src/app/components/category-list/category-list.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../../models/categoryModel/category';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { CategoryService } from '../../services/categoryService/category.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [NgForOf], //ngModule pour la version précedente
+  imports: [NgForOf, FormsModule, NgIf], //ngModule pour la version précedente
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css',
 })
 export class CategoryListComponent implements OnInit {
   categories!: Category[];
+  name!: string;
+  rate: number = 0;
+  updatePost: boolean = false;
+  categoryUpdate!: Category;
+
   constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
@@ -24,12 +30,47 @@ export class CategoryListComponent implements OnInit {
       "fields" : []
     }];
     */
-   this.getCategories(); //execution de la fonction getCategories
+    this.getCategories(); //execution de la fonction getCategories
   }
 
   private getCategories() {
     this.categoryService.getCategoryList().subscribe((data) => {
       this.categories = data;
     });
+  }
+
+  getCategoryFormData(data: Category) {
+    //clear the inputs
+    this.name = '';
+    this.rate = 0;
+    this.categoryService.saveCategory(data).subscribe();
+    this.getCategories();
+  }
+
+  getCategoryFromList(categoryId: number) {
+    this.categories.forEach((value) => {
+      if (value.id == categoryId) {
+        this.name = value.name;
+        this.rate = value.rate;
+        this.categoryUpdate = value;
+      }
+    });
+    this.updatePost = true;
+  }
+
+  updateCategoryFromData(data: Category) {
+    this.name = '';
+    this.rate = 0;
+    this.categoryUpdate.name = data.name;
+    this.categoryUpdate.rate = data.rate;
+
+    this.categoryService.updateCategory(this.categoryUpdate).subscribe();
+    this.updatePost = false;
+    this.getCategories();
+  }
+
+  deleteCategoryFromList(categoryId: number) {
+    this.categoryService.deleteCategory(categoryId).subscribe();
+    this.getCategories();
   }
 }
--- frontend/src/app/services/categoryService/category.service.ts
+++ frontend/src/app/services/categoryService/category.service.ts
@@ -4,15 +4,25 @@ import { Observable } from 'rxjs';
 import { Category } from '../../models/categoryModel/category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
-
-  private baseURL = "http://localhost:8080/api/category";
+  private baseURL = 'http://localhost:8080/api/category';
 
   constructor(private httpClient: HttpClient) {} // il'est pas injecter s'il n'est pas configurer sur le fichier 'app.config.ts'
 
-  getCategoryList(): Observable<Category[]>{
+  getCategoryList(): Observable<Category[]> {
     return this.httpClient.get<Category[]>(`${this.baseURL}`);
+  }
+
+  saveCategory(data: Category) {
+    return this.httpClient.post(`${this.baseURL}`, data);
+  }
+
+  updateCategory(data: Category) {
+    return this.httpClient.put(`${this.baseURL}`, data);
+  }
+  deleteCategory(id: number){
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
   }
 }
